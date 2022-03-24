67ad919d1cc8d6ffc54242494ae33456dfe0a7d3
Julien Jesionek <julien.jesionek@gmail.com>
2022-03-24 16:33:06 +0100

    product page on going

--- front/js/product.js
+++ front/js/product.js
@@ -7,7 +7,6 @@ const url = new URL(str);
 const idProduct = url.searchParams.get("id");
 console.log(idProduct);
 
-
 // Récupération de l'article depuis l'API
 fetch("http://localhost:3000/api/products/" + idProduct)
     .then(function(res) {
@@ -19,6 +18,7 @@ fetch("http://localhost:3000/api/products/" + idProduct)
         const article = showAPI;
         console.table(article);
 
+        // Affichage des éléments du porduit sur la page (même méthode que index)
         let productImg = document.createElement("img");
         document.querySelector(".item__img").appendChild(productImg);
         productImg.src= article.imageUrl;
@@ -33,13 +33,92 @@ fetch("http://localhost:3000/api/products/" + idProduct)
         let productDescription = document.getElementById('description');
         productDescription.innerHTML = article.description;
 
+        // Gestion des couleurs du produit
         for (let colors of article.colors){
-            let productColors = document.createElement("option");
-            document.querySelector("#colors").appendChild(productColors);
-            productColors.value = colors;
-            productColors.innerHTML = colors;
+            let productColor = document.createElement("option");
+            document.querySelector("#colors").appendChild(productColor);
+            productColor.value = colors;
+            productColor.innerHTML = colors;
         }
     })
     .catch(function(error) {
         console.log(error);
+        alert("Un problème technique empêche le chargement" + "des informations du produit, veuillez" + "recharger la page."); // un pop-up s'affiche en cas d'erreur de chargement
     });
+
+// Vérification: y a-t-il quelque chose dans le localstorage?
+const myStockageJson = localStorage.getItem("item");
+const myStockageJs = JSON.parse(myStockageJson);
+
+console.log(myStockageJs);
+
+let myCart=[ { // On imagine le panier comme un array
+        id : " ",
+        quantity: 0,
+        color: " "
+}];    
+
+// Ajout du/des produit(s) au panier
+// Récupération quantité, couleurs & id
+document.getElementById("addToCart").addEventListener("click", function() { // On écoute le clique sur le bouton
+    let quantityPicked = Number(document.getElementById("quantity").value); // Récupération quantité
+    let colorsPicked = document.getElementById("colors").value; // Récupération couleurs
+    let idPicked = idProduct; // Récupération id
+
+    if (quantityPicked > 0 && quantityPicked <= 100 && colorsPicked != "--SVP, choisissez une couleur --") { // Vérification choix de la quantité et des couleurs
+        alert("Vous avez bien choisi une couleur ainsi qu'une quantité pour ce produit.");
+        if(myStockageJs) {
+            if(myStockageJs.lenght === 0) {
+                console.log("le panier est vide");
+                alert("le panier est vide");
+
+                myCart=[ {
+                    id : idPicked,
+                    quantity : quantityPicked,
+                    colors : colorsPicked,
+                }];
+
+                let myCartJson = JSON.stringify(myCart); // On transforme l'objet JS en JSON
+                localStorage.setItem("item", myCartJson); // On le stocke dans le localStorage
+            }else {
+                let addArticle = false;
+                for (let i=0; i<myStockageJs.lenght; i++) { // Ajout de produit tant que i est compris entre 0 et le nombre de colonnes dans l'array
+                    let idProductAlreadyPicked = myStockageJs[i].id;
+                    let colorsProductAlreadyPicked = myStockageJs[i].colors;
+                    if(idProductAlreadyPicked === idPicked && colorsProductAlreadyPicked === colorsPicked) {
+                        myStockageJs[i].quantity = Number(mystockageJS[i].quantity) + quantityPicked;
+                        addArticle = true;
+
+                        myStockageJson = JSON.stringify(myStockageJs);
+                        localStorage.setItem("item", monPanierJson);
+                    }else {
+                        
+                    }
+                }
+                if(addArticle === false){ // ce code ne s'exécute pas si addArticle === true, il s'agit d'ajouter un produit non existant dans le panier
+                    myCart.push( 
+                        {
+                            id : idPicked,
+                            quantity : quantityPicked,
+                            colors : colorsPicked,
+                        }
+                    );
+                    let myCartJson = JSON.stringify(myStockageJs);
+                    localStorage.setItem("item", myCartJson);
+                }
+            }
+            console.table(myCart);
+        }else{
+            myCart=[ {
+                id : idPicked,
+                quantity : quantityPicked,
+                colors : colorsPicked,
+            }];
+            let myCartJson = JSON.stringify(myCart); // On transforme l'objet JS en JSON
+            localStorage.setItem("item", myCartJson); // On le stocke dans le localStorage
+        }
+    }else {
+        alert("Veuillez choisir une couleur ainsi qu'une quantité pour ce produit.")
+        console.log("Choisissez une couleur & une quantité");
+    }
+})
--- front/js/script.js
+++ front/js/script.js
@@ -9,31 +9,31 @@ fetch("http://localhost:3000/api/products")
         const articles = showAPI;
         console.table(articles);
 
-        // boucle 1 article par index
+        // Boucle 1 article par index
         for (let article in articles) {
 
-            // liens dynamiques
+            // Liens dynamiques
             let productLink = document.createElement("a");
             document.querySelector(".items").appendChild(productLink);
             productLink.href = 'product.html?id=' + showAPI[article]._id;
 
-            // articles dynamiques
+            // Articles dynamiques
             let productArticle = document.createElement("article");
             productLink.appendChild(productArticle);
 
-            // imgs dynamiques
+            // Imgs dynamiques
             let productImg = document.createElement("img");
             productArticle.appendChild(productImg);
             productImg.src= showAPI[article].imageUrl;
             productImg.alt= showAPI[article].altTxt;
 
-            // nom de l'article dynamique
+            // Nom de l'article dynamique
             let productName = document.createElement("h3");
             productArticle.appendChild(productName);
             productName.classList = "productName";
             productName.innerHTML = showAPI[article].name;
 
-            // description de l'article
+            // Description de l'article
             let productDescription = document.createElement("p");
             productArticle.appendChild(productDescription);
             productDescription.classList = "productDescription";
